18c1869e66f7de9554cfa776a4e2d95d0a98b174
Michael Hammond <michael.hammond@meltwater.org>
2020-03-16 07:52:35 +0000

    fixed the display of the chatbot

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import { createProvider } from './vue-apollo'
+import { createProvider } from './vue-apollo';
+
+import './assets/css/main.css';
 
 Vue.config.productionTip = false
 Vue.use(Chat);
